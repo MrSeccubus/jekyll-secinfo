88e7a35cd3bf599447e21e6065483e16dfe53b2c
Frank Breedijk <fbreedijk@schubergphilis.com>
2021-02-02 14:31:23 +0100

    Improved rspec and fix config merge

--- lib/jekyll-secinfo/config.rb
+++ lib/jekyll-secinfo/config.rb
@@ -4,43 +4,52 @@
 require "jekyll-secinfo/logger" 
 
 CONFIG_NAME = 'jekyll-secinfo'
-DEFAULT_CONFIG = {
-	CONFIG_NAME => {
-		"cve" => {
-			"style" => "mitre"
-		}
-	}
-}
-
 
 module Jekyll::Secinfo
 	class Config
-		
+				
 		def self.get(site_config, page)
-			config = DEFAULT_CONFIG
-			if site_config
-				config = config.merge(site_config) if site_config.key?(CONFIG_NAME)
-			end
-			if page.key?(CONFIG_NAME)
-				fromdoc = { CONFIG_NAME => page[CONFIG_NAME] }
-				config = config.merge(fromdoc)
-			end
-
-			if not config[CONFIG_NAME]["cve"].key?("url")
-				case config[CONFIG_NAME]["cve"]["style"]
-				when "mitre"
-					config[CONFIG_NAME]["cve"]["url"] = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-"
-				when "cvedetails"
-					config[CONFIG_NAME]["cve"]["url"] = "https://www.cvedetails.com/cve/CVE-%s/"
-				when "nvd"
-					config[CONFIG_NAME]["cve"]["url"] = "https://nvd.nist.gov/vuln/detail/CVE-"
-				else
-					# Unknown CVE style unsing 'mitre'-style instead
-					config[CONFIG_NAME]["cve"]["url"] = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-"
+			config = { "cve" => {} }
+			if site_config && site_config.key?(CONFIG_NAME) 
+				#config["site"] = site_config[CONFIG_NAME]
+				if site_config[CONFIG_NAME].key?("cve") && site_config[CONFIG_NAME]["cve"]
+					if site_config[CONFIG_NAME]["cve"].key?("style") && site_config[CONFIG_NAME]["cve"]["style"]
+						config["cve"]["style"] = site_config[CONFIG_NAME]["cve"]["style"]
+					end
+					if site_config[CONFIG_NAME]["cve"].key?("url") && site_config[CONFIG_NAME]["cve"]["url"]
+						config["cve"]["url"] = site_config[CONFIG_NAME]["cve"]["url"]
+					end
 				end
 			end
 
-			return config[CONFIG_NAME]
+			if page.key?(CONFIG_NAME) && page[CONFIG_NAME]
+	    		if page[CONFIG_NAME].key?("cve") && page[CONFIG_NAME]["cve"]
+	    			if page[CONFIG_NAME]["cve"].key?("style") && page[CONFIG_NAME]["cve"]["style"]
+	    				config["cve"]["style"]=page[CONFIG_NAME]["cve"]["style"]
+	    				config["cve"]["url"]=nil
+		    		end
+	    			if page[CONFIG_NAME]["cve"].key?("url") && page[CONFIG_NAME]["cve"]["url"]
+	    				config["cve"]["url"]=page[CONFIG_NAME]["cve"]["url"]
+	    				config["cve"].delete("style")
+		    		end
+		    	end
+			end			
+
+			if not config["cve"]["url"]  
+				case config["cve"]["style"]
+				when "mitre"
+					config["cve"]["url"] = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-"
+				when "cvedetails"
+					config["cve"]["url"] = "https://www.cvedetails.com/cve/CVE-%s/"
+				when "nvd"
+					config["cve"]["url"] = "https://nvd.nist.gov/vuln/detail/CVE-"
+				else
+					# Unknown CVE style using 'mitre'-style instead
+					config["cve"]["url"] = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-"
+				end
+			end
+
+			return config
 		end #get_config
 
 	end #Config
